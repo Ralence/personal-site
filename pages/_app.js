975469ccb55020e3dcffe81e0b0b951cafb5c713
Ralence <ranko.ostojic@gmail.com>
2021-06-17 18:00:19 +0200

    feat: set up styled-components and start the index page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { blue, GlobalStyle, standard } from "../styles/GlobalStyle";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [theme, setTheme] = useState(standard);
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} setTheme={setTheme} theme={theme} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
