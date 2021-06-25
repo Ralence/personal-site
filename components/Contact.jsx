f969d77c564b74716411274414ce5ef28b963d9f
Ralence <ranko.ostojic@gmail.com>
2021-06-25 23:54:00 +0200

    feat: implement nodemailer and the contact form

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,24 +1,100 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Contact = () => {
+  const {
+    register,
+    handleSubmit,
+    control,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    console.log(data, "subm");
+    try {
+      const response = await fetch(`/api/contact/`, {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        // mode: "cors", // no-cors, *cors, same-origin
+        // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+          "Content-Type": "application/json",
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify({ mail_info: data }), // body data type must match "Content-Type" header
+      });
+
+      const result = await response.json();
+      /*
+        if (result.success) {
+            setLoading(false);
+            setOrderSuccess(true);
+        } else {
+            setLoading(false);
+            setOrderSuccess(false);
+        } */
+
+      console.log("ordered", result); // parses JSON response into native JavaScript objects
+    } catch (error) {
+      console.log(error);
+      // setLoading(false);
+      // setOrderSuccess(false);
+    }
+  };
+
+  console.log(watch("name"));
   return (
     <section className="s2">
       <div className="main-container">
         <h3 style={{ "text-align": "center" }}>Get In Touch</h3>
 
-        <form action="" className="contact-form">
-          <label htmlFor="name">Name</label>
-          <input className="input-field" type="text" name="name" />
+        <form onSubmit={handleSubmit(onSubmit)} className="contact-form">
+          <label>Name</label>
+          <input
+            className="input-field"
+            type="text"
+            name="name"
+            {...register("name", { required: true })}
+          />
+          {errors.name?.type === "required" && (
+            <p style={{ color: "#f00", margin: 0 }}>Name is required</p>
+          )}
 
-          <label htmlFor="subject">Subject</label>
-          <input className="input-field" type="text" name="subject" />
+          <label>Subject</label>
+          <input
+            className="input-field"
+            type="text"
+            name="subject"
+            {...register("subject", { required: true })}
+          />
+          {errors.subject?.type === "required" && (
+            <p style={{ color: "#f00", margin: 0 }}>Subject is required</p>
+          )}
 
-          <label htmlFor="email">Email</label>
-          <input className="input-field" type="email" name="email" />
+          <label>Email</label>
+          <input
+            className="input-field"
+            type="email"
+            name="email"
+            {...register("email", { required: true })}
+          />
+          {errors.email?.type === "required" && (
+            <p style={{ color: "#f00", margin: 0 }}>Email is required</p>
+          )}
 
           <label>Message</label>
-          <textarea className="input-field" name="message"></textarea>
-          <input className="submit-btn" type="submit" value="send" />
+          <textarea
+            className="input-field"
+            name="message"
+            {...register("message", { required: true })}
+          ></textarea>
+          {errors.message?.type === "required" && (
+            <p style={{ color: "#f00", margin: 0 }}>Message is required</p>
+          )}
+          <input className="submit-btn" type="submit" />
         </form>
       </div>
     </section>
---
+++ pages/api/contact.js
@@ -0,0 +1,66 @@
+import nc from "next-connect";
+var nodemailer = require("nodemailer");
+
+const renderHTML = (message) => {
+  const html = `<!DOCTYPE html>
+    <html>
+    <head>
+    <title>Message</title>
+   
+  </head>
+  <body>
+      <header>
+        <h1>${message.title}</h1>
+
+        <h3>Subject: ${message.mail_info.subject}</h3>
+
+        <h3>From: ${message.mail_info.name}</h3>
+        <h4>${message.mail_info.email}</h4>
+
+        <p>${message.mail_info.message}</p>
+    </header>
+    <main>
+    </main>
+  </body>
+    </html>
+    `;
+
+  return html;
+};
+
+var transporter = nodemailer.createTransport({
+  service: "gmail",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_ADDRESS,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+const handler = nc().post(async (req, res) => {
+  const message = {
+    ...req.body,
+    title: "MY SITE CONTACT",
+    id: Date.now(),
+  };
+
+  var mailOptions = {
+    from: process.env.EMAIL_ADDRESS,
+    to: process.env.TARGET_MAIL,
+    subject: "Message from my site",
+    text: JSON.stringify(message),
+    html: renderHTML(message),
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      res.status(500).json({ error: error });
+    } else {
+      res.send(
+        JSON.stringify({ success: true, message: "Message sent. Thanks for reaching out!" })
+      );
+    }
+  });
+});
+export default handler;
