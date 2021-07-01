a0ae848a60a5cc518e05f44356fc438e93a4e781
Ralence <ranko.ostojic@gmail.com>
2021-07-01 10:02:44 +0200

    fix: import path

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Intro from "../components/intro";
+import Intro from "../components/Intro";
 import About from "../components/About";
 import Projects from "../components/Projects";
 import Contact from "../components/Contact";
